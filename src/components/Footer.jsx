42b0ecb955499f7480fcc14d3b4f28e1eb03f801
talhashaikh-tech <talhashaikh1155@gmail.com>
2025-08-01 14:38:08 +0530

    final version

---
+++ src/components/Footer.jsx
@@ -0,0 +1,50 @@
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer_container">
+        <div className="footer_column">
+          <h3>ONLINE SHOPPING</h3>
+
+          <a href="#">Men</a>
+          <a href="#">Women</a>
+          <a href="#">Kids</a>
+          <a href="#">Home & Living</a>
+          <a href="#">Beauty</a>
+          <a href="#">Gift Card</a>
+          <a href="#">Myntra Insider</a>
+        </div>
+
+        <div className="footer_column">
+          <h3>USEFUL LINKS</h3>
+
+          <a href="#">Blog</a>
+          <a href="#">Careers</a>
+          <a href="#">Site Map</a>
+          <a href="#">Corporate Information</a>
+          <a href="#">Whitehat</a>
+          <a href="#">Cleartrip</a>
+          <a href="#">Myntra Global</a>
+        </div>
+
+        <div className="footer_column">
+          <h3>CUSTOMER POLICIES</h3>
+
+          <a href="#">Contact Us</a>
+          <a href="#">T&C</a>
+          <a href="#">Track Orders</a>
+          <a href="#">Shipping</a>
+          <a href="#">Returns</a>
+          <a href="#">Cancellation</a>
+          <a href="#">Privacy Policy</a>
+        </div>
+      </div>
+      <hr />
+
+      <div className="copyright">
+        © 2025 www.myntra.com. All rights reserved.
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
